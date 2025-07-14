2cfbddf1593a1461735a8f091df7c060223a901c
Steven Gong <gonghaima@hotmail.com>
2025-07-14 14:04:25 +1000

    add Safety page

--- src/app/stages/safety/safety.ts
+++ src/app/stages/safety/safety.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-safety',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './safety.html',
   styleUrl: './safety.scss'
 })
 export class Safety {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.form = this.fb.group({
+      safetyAnswer: [undefined]
+    });
+  }
+
+  get safetyAnswer() {
+    return this.form.get('safetyAnswer')?.value;
+  }
+
+  goToLanding() {
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      this.router.navigate(['']);
+    }
+  }
 }
