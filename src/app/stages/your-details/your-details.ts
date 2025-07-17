8cb550bf39cd9ac6c4876e067712e75ef649b7b6
Steven Gong <gonghaima@hotmail.com>
2025-07-17 18:03:05 +1000

    add your-details page

--- src/app/stages/your-details/your-details.ts
+++ src/app/stages/your-details/your-details.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-your-details',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './your-details.html',
   styleUrl: './your-details.scss'
 })
 export class YourDetails {
+  form: FormGroup;
+  titleOptions = ['Mr', 'Mrs', 'Ms', 'Miss', 'Dr', 'Other'];
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: [''],
+      firstName: ['', Validators.required],
+      middleName: [''],
+      lastName: ['', Validators.required],
+      previousLastName: [undefined, Validators.required],
+      preferredName: [undefined, Validators.required],
+      dateOfBirth: ['', Validators.required],
+      gender: [undefined, Validators.required],
+      tfn: [''],
+      csrn: [undefined, Validators.required],
+      crn: [undefined, Validators.required],
+      hadCase: [undefined, Validators.required],
+    });
+  }
+
+  get f() { return this.form.controls; }
 }
